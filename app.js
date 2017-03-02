05076b4e95163d2b5f5adae2600da392d1a6883b
Ela Neska <ela.neska@wp.pl>
2017-03-02 17:55:04 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 var buttonR = document.getElementById("right");
-
+var buttonL = document.getElementById("left");
+var i=0;
 buttonR.addEventListener("click", function rightGallery() {
 
-    for (var i=0; i<10; i++) {
-
         var start = document.getElementById("zdj");
-        var image ="<img src=\"img/hex" + i + ".png\">";
 
+        var i_right= i+1;
+        if (i>8) i_right=1;
+
+        var image ="<img src=\"img/hex" + i_right + ".png\">";
         start.innerHTML = image;
-    }
+
+        i = i_right;
+
 })
+
+
+buttonL.addEventListener("click", function leftGallery() {
+
+    var start = document.getElementById("zdj");
+
+    var i_left =i-1;
+    if (i<1) i_left=9;
+
+    var image ="<img src=\"img/hex" + i_left + ".png\">";
+    start.innerHTML = image;
+
+    i = i_left;
+
+})
+
 
 
 
